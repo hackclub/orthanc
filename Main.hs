506603bab668098393c55e7ef80244e509317f39
Benjamin Summers <ben@benjaminsummers.net>
2014-11-24 04:24:34 +0000

    Work stealing with strings.

--- Main.hs
+++ Main.hs
@@ -13,9 +13,8 @@ replyBack (sender, msg) = send sender msg
 logMessage :: String -> Process ()
 logMessage msg = say $ "handling " ++ msg
 
-githubName :: String -> String
-githubName username =
-	"Zach Latta"
+githubName :: String -> IO String
+githubName username = return username
 
 slave :: (ProcessId, ProcessId) -> Process ()
 slave (master, workQueue) = do
@@ -24,9 +23,8 @@ slave (master, workQueue) = do
 	where
 		go us = do
 			send workQueue us
-
 			receiveWait
-				[ match $ \username  -> send master (githubName username) >> go us
+				[ match $ \nm -> liftIO(githubName nm) >>= send master >> go us
 				, match $ \() -> return ()
 				]
 
@@ -43,23 +41,12 @@ accStrings = go []
 master :: [String] -> [NodeId] -> Process [String]
 master usernames slaves = do
 	us <- getSelfPid
-
 	workQueue <- spawnLocal $ do
-		-- Reply with the next bit of work to be done
-		sequence $ f usernames
-			where f u = do
-				them <- expect
-				send them u
-		--  Once all the work is done, tell the slaves to terminate
-		forever $ do
-			pid <- expect
-			send pid ()
+		sequence $ map (\u → expect >>= \them→send them u) usernames
+		forever $ expect >>= \pid → send pid ()
 
-	-- Start slave processes
 	forM_ slaves $ \nid -> spawn nid ($(mkClosure 'slave) (us, workQueue))
-
-	-- Wait for the result
-	accStrings (fromIntegral n)
+	accStrings $ length usernames
 
 rtable :: RemoteTable
 rtable = __remoteTable initRemoteTable
@@ -74,6 +61,9 @@ main = do
 			startMaster backend $ \slaves -> do
 				result <- master (read n) slaves
 				liftIO $ print result
+
 		["slave", host, port] -> do
 			backend <- initializeBackend host port rtable
 			startSlave backend
+
+		_ -> putStrLn "Get your shit together"
